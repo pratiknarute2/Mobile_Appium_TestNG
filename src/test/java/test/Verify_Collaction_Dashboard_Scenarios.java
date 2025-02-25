f136823dc412b2ce377620ead9626b4a023321d8
testingautomation <sridhar.p@qualitrix.com>
2025-02-25 09:50:22 +0530

    Removed leaked GitHub token

--- src/test/java/test/Verify_Collaction_Dashboard_Scenarios.java
+++ src/test/java/test/Verify_Collaction_Dashboard_Scenarios.java
@@ -6,9 +6,10 @@ import pom.CollectionDashboard;
 
 public class Verify_Collaction_Dashboard_Scenarios extends BaseClass {
 
-    @Test(priority = 1, enabled = false)
+    @Test(enabled = false)
     public void verify_collection_dashboard_scenarios() throws InterruptedException {
         new CollectionDashboard().handle_customer_name_dropdown_list();
+
     }
 
 }
