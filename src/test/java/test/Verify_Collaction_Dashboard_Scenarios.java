15619862b150453ddc2ab4b53f45c7dae7bc70dc
testingautomation <sridhar.p@qualitrix.com>
2025-02-25 09:12:55 +0530

    disable verify_collection_dashboard_scenarios method to execute in jenkins

--- src/test/java/test/Verify_Collaction_Dashboard_Scenarios.java
+++ src/test/java/test/Verify_Collaction_Dashboard_Scenarios.java
@@ -6,8 +6,8 @@ import pom.CollectionDashboard;
 
 public class Verify_Collaction_Dashboard_Scenarios extends BaseClass {
 
-    @Test(priority = 1)
-    public void verify_collection_dashboard_scenarioss() throws InterruptedException {
+    @Test(priority = 1, enabled = false)
+    public void verify_collection_dashboard_scenarios() throws InterruptedException {
         new CollectionDashboard().handle_customer_name_dropdown_list();
     }
 
